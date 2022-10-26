438f515340ec1ff515dcc82ba7ad3d98a7d6ec9d
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-26 11:04:50 +0800

    Update firebase.ts

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -18,7 +18,7 @@ const firebaseConfig = {
   storageBucket: "topmovie-cd7f7.appspot.com",
   messagingSenderId: "552388803144",
   appId: "1:552388803144:web:5c2adec1c48519035e767d",
-  measurementId: "G-FKRY4VMDFY"
+
 };
 
 // const firebaseConfig = {
