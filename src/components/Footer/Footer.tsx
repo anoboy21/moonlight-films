29ac6ca23eb7bd050f680cd326401a3f27f18889
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-26 11:44:34 +0800

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,21 +8,13 @@ const Footer: FunctionComponent<FooterProps> = () => {
   return (
     <div className="bg-dark-lighten text-white flex justify-between items-center py-3 px-4 shadow-md mt-3">
       <p className="flex gap-2">
-        <span>Copyright TopMovie</span>
+        <span>Copyright </span>
         <span className="hidden md:block"> &copy; 2022</span>
       </p>
       <div className="flex gap-3 items-center">
         <p className="hidden md:block">Contact me: </p>
         <div className="flex gap-2">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:text-[#6e5494] transition duration-300"
-          >
-            <AiFillGithub size={25} />
-          </a>
-          <a
+                    <a
             href="https://www.facebook.com/"
             target="_blank"
             rel="noreferrer"
