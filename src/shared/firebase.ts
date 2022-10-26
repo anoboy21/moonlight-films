48e1fb1531219e867feedbccc50e0f232b424a31
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-26 16:22:31 +0800

    Update firebase.ts

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -14,6 +14,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyBfRFncIkXLmKSWx8efBT-RcUiDmaCr6Gc",
   authDomain: "topmovie-cd7f7.firebaseapp.com",
+  databaseURL: "https://topmovie-cd7f7-default-rtdb.firebaseio.com",
   projectId: "topmovie-cd7f7",
   storageBucket: "topmovie-cd7f7.appspot.com",
   messagingSenderId: "552388803144",
