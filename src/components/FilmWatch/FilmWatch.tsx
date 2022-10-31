65c613ec5dd74cb9613413f8473b344d187933a4
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-31 16:44:22 +0800

    Update FilmWatch.tsx

--- src/components/FilmWatch/FilmWatch.tsx
+++ src/components/FilmWatch/FilmWatch.tsx
@@ -251,7 +251,7 @@ const FilmWatch: FunctionComponent<FilmWatchProps & getWatchReturnedType> = ({
               </ReadMore>
             )}
           </div>
-          <Comment media_type={media_type} id={detail?.id} />
+          
         </div>
         <div className="shrink-0 md:max-w-[400px] w-full relative px-6">
           {!isMobile && <SearchBox />}
